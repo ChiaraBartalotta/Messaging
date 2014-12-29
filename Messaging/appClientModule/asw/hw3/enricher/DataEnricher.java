f940ab039637418a4dc8205c9d3db97f2b5b8be3
godofwar91 <davidebernardini91@gmail.com>
2014-12-29 12:26:35 +0100

    aggiunta la classe dataenricher

---
+++ Messaging/appClientModule/asw/hw3/enricher/DataEnricher.java
@@ -0,0 +1,19 @@
+package asw.hw3.enricher;
+
+import javax.jms.*;
+
+public class DataEnricher {
+	
+	private Queue codaOrdiniSenzaId;
+	private int idOrder;
+	
+	public DataEnricher() {
+		this.idOrder = 0;
+	}
+	
+	public void assignIdOrder(String order) {
+		
+	}
+	 
+
+}
