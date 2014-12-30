8c339c971d126597b4e54623f3985cf531ef4dcf
godofwar91 <davidebernardini91@gmail.com>
2014-12-30 16:42:37 +0100

    aggiunto il json in produttoreOrdini

--- Messaging/appClientModule/asw/hw3/produttoreordini/ProduttoreOrdini.java
+++ Messaging/appClientModule/asw/hw3/produttoreordini/ProduttoreOrdini.java
@@ -1,13 +1,17 @@
 package asw.hw3.produttoreordini;
 
+import java.io.IOException;
 import java.util.logging.Logger;
 
 import javax.jms.ConnectionFactory;
 import javax.jms.Queue;
 
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
 import asw.hw3.dominio.GeneratoreOrdini;
 import asw.hw3.dominio.Ordine;
-
 import asw.jms.simpleproducer.SimpleProducer;
 import asw.util.logging.AswLogger;
 
@@ -55,9 +59,23 @@ public class ProduttoreOrdini {
 				};
 		GeneratoreOrdini g = new GeneratoreOrdini(clienti, prodotti); 
 		for (int i=0; i<100; i++) {
-			Ordine o = g.getRandomOrdine(); 
+			Ordine o = g.getRandomOrdine();
+			String jsonOrder = "";
 			logger.info("Creo ordine (senza id): " + o.toString());
-			p.sendMessage(o.toString()); 
+			ObjectMapper mapper = new ObjectMapper();
+			try {
+				jsonOrder = mapper.writeValueAsString(o);
+			} catch (JsonGenerationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			p.sendMessage(jsonOrder); 
 		}
 		
 		/* disconnette il produttore */ 
